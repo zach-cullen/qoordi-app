a3d789e53e9da4334a23232e5bf538f677b972ab
Zach Cullen <zacullen@gmail.com>
2020-06-11 11:41:57 -0500

    add validations to user model

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, presence: true, uniqueness: true, case_sensitive: false
+  validates :given_name, :family_name, presence: true
 end
