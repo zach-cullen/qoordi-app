4a4d731ce7982c1c88a04019a0ef71507f7ac6d8
Zach Cullen <zacullen@gmail.com>
2020-06-11 13:58:37 -0500

    add default route and redefine home route

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import Home from '../components/Home'
 import Login from '../components/Login'
 import Signup from '../components/Signup'
@@ -8,10 +8,13 @@ import UserContainer from '../containers/UserContainer'
 const Routes = () => {
   return(
     <Switch>
-      <Route exact path="/" component={Home}/>
+      <Route exact path="/home" component={Home}/>
       <Route exact path="/login" component={Login} />
       <Route exact path="/signup" component={Signup} />
       <Route exact path="/app" component={UserContainer} />
+      <Route path="/">
+        <Redirect exact to="/home" />
+      </Route>
     </Switch>
   )
 }
