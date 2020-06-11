85b482f7eaa2764b146fd0429edece665f0214ae
Zach Cullen <zacullen@gmail.com>
2020-06-11 14:16:01 -0500

    render LoginForm in Login component

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import LoginForm from './forms/LoginForm'
 
 const Login = () => {
+
   return(
     <div id="login">
       <h1>Log In</h1>
+      <LoginForm />
     </div>
   )
 }
