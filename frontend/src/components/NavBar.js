f4340279509369734ace556f2381aced4f4a97fe
Zach Cullen <zacullen@gmail.com>
2020-06-11 14:04:27 -0500

    change dashboard link text to dash

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = () => {
   return(
     <nav>
       <NavLink exact to="/home">Home </NavLink>
-      <NavLink exact to="/app">Dashboard </NavLink>
+      <NavLink exact to="/app">Dash </NavLink>
       <NavLink exact to="/login">Login </NavLink>
       <NavLink exact to="/signup">Signup </NavLink>
     </nav>
