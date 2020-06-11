fd1dba7c284e5883d601374ad1552a85249c0c28
Zach Cullen <zacullen@gmail.com>
2020-06-11 13:58:53 -0500

    redefine path home link

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const NavBar = () => {
   return(
     <nav>
-      <NavLink exact to="/">Home </NavLink>
+      <NavLink exact to="/home">Home </NavLink>
       <NavLink exact to="/app">Dashboard </NavLink>
       <NavLink exact to="/login">Login </NavLink>
       <NavLink exact to="/signup">Signup </NavLink>
