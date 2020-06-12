f02efebbe3a65115e5c9c4c13ffc38895c33b757
Zach Cullen <zacullen@gmail.com>
2020-06-12 14:06:00 -0500

    post login credentials to api and log json response

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -3,8 +3,26 @@ authService is responsible for fetching authentication data from the api and
 calling functions from the sessionService library to affect the sessionReducer via side-effect
 */
 
+import { API_URL } from '../store/constants'
+
+const headers = {
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  }
+}
+
 export const logInUser = (credentials = {email: '', password: ''}) => {
 
-  console.log("authService: ", credentials)
+  let payload = {
+    ...headers,
+    method: "POST",
+    body: JSON.stringify(credentials)
+  }
 
+  console.log("about to fetch: ", payload)
+
+  return fetch(`${API_URL}/login`, payload)
+    .then(res => res.json())
+    .then(json => console.log("response: ", json))
 }
