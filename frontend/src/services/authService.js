c242d176fa89f119dbc2a17ef446f4e19035d843
Zach Cullen <zacullen@gmail.com>
2020-06-12 15:09:41 -0500

    call sessionService functions to handle json response for loginUser

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,3 +1,5 @@
+import { sessionService } from 'redux-react-session'
+
 /* 
 authService is responsible for fetching authentication data from the api and 
 calling functions from the sessionService library to affect the sessionReducer via side-effect
@@ -22,5 +24,21 @@ export const logInUser = (user) => {
 
   return fetch(`${API_URL}/login`, payload)
     .then(res => res.json())
-    .then(json => console.log("response: ", json))
+    .then(json => {
+      let login = () => {
+        // sets redux session state authenticated to true
+        sessionService.saveSession()
+        // populates user object in redux session state
+        sessionService.saveUser(json.user)
+      }
+      
+      let error = () => {
+        sessionService.deleteUser()
+        sessionService.deleteSession()
+        console.log(json.errors)
+      }
+      
+      json.authenticated ? login() : error()
+
+    })
 }
