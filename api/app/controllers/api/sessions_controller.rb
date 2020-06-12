7cd1d11d9146ebd4a7e682ef81fb9831aaad56c9
Zach Cullen <zacullen@gmail.com>
2020-06-12 14:30:40 -0500

    find and validate user with login credentials and render json response

--- api/app/controllers/api/sessions_controller.rb
+++ api/app/controllers/api/sessions_controller.rb
@@ -1,8 +1,25 @@
 class Api::SessionsController < ApplicationController
 
   def login
-    byebug
-    render json: { message: "I hear you"}
+    @user = User
+      .find_by(email: params[:user][:email])
+      .try(:authenticate, params[:user][:password])
+
+    if @user
+      render json: {
+        authenticated: true,
+        user: @user.as_json(only: [:id])
+      }
+    else 
+      render json: login_error
+    end
+
+  end
+
+  private 
+
+  def login_error
+    { errors: ["Unable to login with the provided email & password. Please sign up or try again."]}
   end
 
 end
