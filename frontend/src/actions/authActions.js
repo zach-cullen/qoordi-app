11be5ac014aea1a6666d506cf9c628de3e26bdad
Zach Cullen <zacullen@gmail.com>
2020-06-12 11:09:30 -0500

    delete on rename

--- frontend/src/actions/authActions.js
+++
@@ -1,5 +0,0 @@
-// import { API_ADDRESS } from '../store/constants'
-
-export const logInUser = (credentials = {email: '', password: ''}) => {
-  console.log(`log in with credentials: `, credentials)
-}
