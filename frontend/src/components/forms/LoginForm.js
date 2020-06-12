7f1a4997628ad3ce1b8b5558a8b43aa0016c6428
Zach Cullen <zacullen@gmail.com>
2020-06-12 11:08:27 -0500

    construct loginform with props

--- frontend/src/components/forms/LoginForm.js
+++ frontend/src/components/forms/LoginForm.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 
 class LoginForm extends Component {
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       email: '',
       password: ''
