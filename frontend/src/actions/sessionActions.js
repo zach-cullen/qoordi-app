f9e2abec4c65e137abaa5dfe1f666e2efc7bf811
Zach Cullen <zacullen@gmail.com>
2020-06-12 12:30:06 -0500

    remove console log

--- frontend/src/actions/sessionActions.js
+++ frontend/src/actions/sessionActions.js
@@ -1,7 +1,6 @@
 // import { API_ADDRESS } from '../store/constants'
 
 export const logInUser = (credentials = {email: '', password: ''}) => {
-  console.log(`log in with credentials: `, credentials)
   return {
     type: 'LOGIN_USER',
     payload: credentials
