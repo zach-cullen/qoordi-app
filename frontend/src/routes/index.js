5a3792135c23303a2ffb9dbefd1746bbdb20b2d1
Zach Cullen <zacullen@gmail.com>
2020-06-12 15:30:18 -0500

    connect Router to redux session and redirect based on authenticated

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,22 +1,37 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import Home from '../components/Home'
 import LoginContainer from '../containers/LoginContainer'
 import SignupContainer from '../containers/SignupContainer'
 import UserContainer from '../containers/UserContainer'
 
-const Routes = () => {
+const Routes = ({ session }) => {
+
+  const loggedIn = session.authenticated
+
   return(
     <Switch>
-      <Route exact path="/home" component={Home}/>
-      <Route exact path="/login" component={LoginContainer} />
-      <Route exact path="/signup" component={SignupContainer} />
-      <Route exact path="/app" component={UserContainer} />
+      <Route exact path="/home" component={Home} />
+
+      <Route exact path="/login">
+        { loggedIn ? <Redirect to="/home" /> : <LoginContainer /> }
+      </Route>
+
+      <Route exact path="/signup">
+        { loggedIn ? <Redirect to="/home" /> : <SignupContainer /> }
+      </Route>
+
+      <Route exact path="/app">
+        { loggedIn ? < UserContainer /> : <Redirect to="/home" /> }
+      </Route>
+
       <Route path="/">
         <Redirect exact to="/home" />
       </Route>
+
     </Switch>
   )
 }
 
-export default Routes
+export default connect(state => ({ session: state.session }))(Routes)
