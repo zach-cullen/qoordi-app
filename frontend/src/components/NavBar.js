d07a3a341ed317496d5cf44b0e9b79f4fb6cf2e8
Zach Cullen <zacullen@gmail.com>
2020-06-12 17:52:24 -0500

    render different navbars based on redux session state

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,15 +1,32 @@
 import React from 'react'
+import { connect} from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
-const NavBar = () => {
-  return(
-    <nav>
-      <NavLink exact to="/home">Home </NavLink>
-      <NavLink exact to="/app">Dash </NavLink>
-      <NavLink exact to="/login">Login </NavLink>
-      <NavLink exact to="/signup">Signup </NavLink>
-    </nav>
-  )
+const NavBar = ({ session }) => {
+
+  const loggedIn = () => session.authenticated
+
+  const loggedInNav = () => {
+    return(
+      <nav>
+        <NavLink exact to="/home">Home </NavLink>
+        <NavLink exact to="/app">Dash </NavLink>
+        <NavLink exact to="/logout">Logout </NavLink>
+      </nav>
+    )
+  }
+
+  const loggedOutNav = () => {
+    return(
+      <nav>
+        <NavLink exact to="/home">Home </NavLink>
+        <NavLink exact to="/login">Login </NavLink>
+        <NavLink exact to="/signup">Signup </NavLink>
+      </nav>
+    )
+  }
+
+  return loggedIn() ? loggedInNav() : loggedOutNav()
 }
 
-export default NavBar
+export default connect(state => ({session: state.session}))(NavBar)
