01bbf4fff898beeb506d88d3b6efc0d6a04c038d
Zach Cullen <zacullen@gmail.com>
2020-06-13 11:12:32 -0500

    render signup form from container

--- frontend/src/containers/SignupContainer.js
+++ frontend/src/containers/SignupContainer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import SignupForm from '../components/forms/SignupForm'
 
 const SignupContainer = () => {
   return(
     <div id="signup">
       <h1>Sign Up</h1>
+      <SignupForm />
     </div>
   )
 }
