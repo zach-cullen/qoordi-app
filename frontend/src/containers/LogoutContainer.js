5caa94ba8320ff70ed471ecdd3db049f3667bf92
Zach Cullen <zacullen@gmail.com>
2020-06-13 10:32:57 -0500

    add comments to functions

--- frontend/src/containers/LogoutContainer.js
+++ frontend/src/containers/LogoutContainer.js
@@ -4,8 +4,10 @@ import { logOutUser } from '../services/authService'
 
 const LogoutContainer = ({ session }) => {
 
+  // calls authService action immediately on load of this component that sends logout request to api
   logOutUser({id: session.user.id})
 
+  // this container renders a temporary element until successful logout action updates state and causes router to redirect
   return(
     <div id="logout">
       <h1>Logging out user...</h1>
