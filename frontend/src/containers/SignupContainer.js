7ef45c3d712b1ebe8687d43ce29ef7af19c95ca6
Zach Cullen <zacullen@gmail.com>
2020-06-13 11:21:44 -0500

    convert from functional to class component and pass down propfunction to signup form

--- frontend/src/containers/SignupContainer.js
+++ frontend/src/containers/SignupContainer.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 import SignupForm from '../components/forms/SignupForm'
 
-const SignupContainer = () => {
-  return(
-    <div id="signup">
-      <h1>Sign Up</h1>
-      <SignupForm />
-    </div>
-  )
+class SignupContainer extends Component {
+
+  submitSignup = (formData) => {
+    console.log("submitting info: ", formData )
+  }
+
+  render() {
+    return(
+      <div id="signup">
+        <h1>Sign Up</h1>
+        <SignupForm propFunction={this.submitSignup} />
+      </div>
+    )
+  }
 }
 
 export default SignupContainer
