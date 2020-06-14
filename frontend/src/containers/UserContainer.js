7434f8e0586e698a2e1327b3ecaa67a1d8ce767e
Zach Cullen <zacullen@gmail.com>
2020-06-14 18:41:48 -0500

    convert UserContainer from functional to class component

--- frontend/src/containers/UserContainer.js
+++ frontend/src/containers/UserContainer.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const UserContainer = () => {
-  return(
-    <div id="user-container">
-      <h1>User</h1>
-    </div>
-  )
+class UserContainer extends Component {
+
+  render() {
+    return(
+      <div id="user-container">
+        <h1>User</h1>
+      </div>
+    )
+  }
 }
 
 export default UserContainer
