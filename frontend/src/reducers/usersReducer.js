955a6694501bf100d27a110489aefd77d2135eb9
Zach Cullen <zacullen@gmail.com>
2020-06-15 10:42:47 -0500

    ADD_USER normalizes user data and updates state

--- frontend/src/reducers/usersReducer.js
+++ frontend/src/reducers/usersReducer.js
@@ -5,12 +5,18 @@ const usersReducer = (state = {
 }, action) => {
 
   switch(action.type) {
-
     case "ADD_USER":
+      const userData = action.payload.user
 
-      console.log(action.payload)
-
-      return state
+      console.log("action payload", userData)
+      return {
+        ...state,
+        byId: {
+          ...state.byId,
+          [userData.id]: userData
+        },
+        allIds: state.allIds.includes(userData.id) ? state.allIds : state.allIds.concat(userData.id)
+      }
 
     default:
       return state
