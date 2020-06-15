106ab7d94ddb6a3f2f70f030bc6420a81d9cd452
Zach Cullen <zacullen@gmail.com>
2020-06-15 11:58:20 -0500

    display user name when loaded

--- frontend/src/containers/UserContainer.js
+++ frontend/src/containers/UserContainer.js
@@ -8,11 +8,28 @@ class UserContainer extends Component {
     this.props.fetchUser(this.props.session.user)
   }
 
+  userIsLoaded = () => {
+    const allUserIds = this.props.entities.users.allIds
+    if (allUserIds.length > 0) {
+      return allUserIds.includes(this.props.session.user.id)
+    }
+    return false
+  }
+
+  renderUserName = () => {
+    if (this.userIsLoaded()) {
+      const currentUser = this.props.entities.users.byId[this.props.session.user.id]
+      return(
+        <h2>{ currentUser.given_name }</h2>
+      )
+    }
+  }
 
   render() {
     return(
       <div id="user-container">
         <h1>User</h1>
+        { this.renderUserName() }
       </div>
     )
   }
@@ -21,6 +38,7 @@ class UserContainer extends Component {
 const mapStateToProps = (state) => {
   return {
     session: state.session,
+    entities: state.entities
   }
 }
 
