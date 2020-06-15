a7ed26e29f094f45b58a2eb02461f939e5efb733
Zach Cullen <zacullen@gmail.com>
2020-06-15 09:08:44 -0500

    add entitiesReducer to redux store

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux'
 import { sessionReducer } from 'redux-react-session'
+import entitiesReducer from './entitiesReducer'
+
 
 const rootReducer = combineReducers({
-  session: sessionReducer
+  session: sessionReducer,
+  entities: entitiesReducer,
 })
 
 export default rootReducer
