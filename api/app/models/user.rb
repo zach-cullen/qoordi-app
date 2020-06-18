1af0bff39d76e4fba7308c700380cdcaf8a728c1
Zach Cullen <zacullen@gmail.com>
2020-06-18 12:24:37 -0500

    build associations between user and project models using join

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true, case_sensitive: false
   validates :given_name, :family_name, presence: true
+
+  has_many :user_projects
+  has_many :projects, through: :user_projects
 end
