5454b2d9f46519783886f0ae4d5213d7ddd5573d
Zach Cullen <zacullen@gmail.com>
2020-06-19 15:17:01 -0500

    render project board headers and rows as child components

--- frontend/src/components/ProjectBoard/ProjectBoard.js
+++ frontend/src/components/ProjectBoard/ProjectBoard.js
@@ -1,9 +1,13 @@
 import './ProjectBoard.css'
 import React from 'react'
+import ProjectBoardHeaders from './ProjectBoardHeaders'
+import ProjectBoardRows from './ProjectBoardRows'
 
 const ProjectBoard = () => {
   return (
     <div id="project-board">
+      <ProjectBoardHeaders />
+      <ProjectBoardRows />
     </div>
   )
 }
