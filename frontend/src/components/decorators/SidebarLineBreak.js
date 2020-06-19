8f1a1119ff863b64886e39cd7afc3052992aaf8b
Zach Cullen <zacullen@gmail.com>
2020-06-19 13:41:00 -0500

    create decorators directory and make component SidebarLineBreak

---
+++ frontend/src/components/decorators/SidebarLineBreak.js
@@ -0,0 +1,11 @@
+import './decorators.css'
+import React from 'react'
+
+const SidebarLineBreak = () => {
+  return (
+    <div className="sidebar-line-break">
+    </div>
+  )
+}
+
+export default SidebarLineBreak
