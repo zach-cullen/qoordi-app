83be405d368aacba827aeea710427f9edb4f9998
Zach Cullen <zacullen@gmail.com>
2020-06-19 15:14:26 -0500

    create component for rendering project board column headers

---
+++ frontend/src/components/ProjectBoard/ProjectBoardHeaders.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ProjectBoardHeaders = () => {
+  return (
+    <div className="project-row-columns" id="project-board-headers">
+      <div>
+        PROJECT
+      </div>
+      <div>
+        DATE
+      </div>
+      <div>
+        CATEGORY
+      </div>
+    </div>
+  )
+}
+
+export default ProjectBoardHeaders
