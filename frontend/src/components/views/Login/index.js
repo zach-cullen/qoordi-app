ff503ce29a20ce8a92e9f48d336696044de61028
Zach Cullen <zacullen@gmail.com>
2020-06-20 17:24:16 -0500

    move LoginForm and SignupForm to their associated view directory

--- frontend/src/components/views/Login/index.js
+++ frontend/src/components/views/Login/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import LoginForm from '../../forms/LoginForm'
+import LoginForm from './LoginForm'
 
 const Login = (props) => {
   return(
--- frontend/src/components/views/Signup/index.js
+++ frontend/src/components/views/Signup/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import SignupForm from '../../forms/SignupForm'
+import SignupForm from './SignupForm'
 
 const Signup = (props) => {
   return(
