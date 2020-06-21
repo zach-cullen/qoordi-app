35b637983082dfd3c734c7002ffdc30191be502e
Zach Cullen <zacullen@gmail.com>
2020-06-21 13:00:52 -0500

    clean out Dashboard view and delete associated styles

--- frontend/src/components/views/Dashboard/index.js
+++ frontend/src/components/views/Dashboard/index.js
@@ -1,21 +1,12 @@
 import './Dashboard.css'
 import React from 'react'
-import DashGreeting from './DashGreeting'
-import ProjectTableTools from './ProjectTable/ProjectTableTools'
-import ProjectTable from './ProjectTable/ProjectTable'
 
 
 const Dashboard = (props) => {
 
   return(
-    <div className="main-container" id="dashboard-grid">
-      <div id="dashboard-tools">
-        <DashGreeting currentUser={props.currentUser}/>
-      </div>
-      <div id="dashboard-main">
-        <ProjectTableTools />
-        <ProjectTable />
-      </div>
+    <div>
+      Dashboard
     </div>
   )
 }
