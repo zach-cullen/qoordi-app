2936cd5520c5377d78276ef89eef7e3d1b9a43f1
Zach Cullen <zacullen@gmail.com>
2020-06-21 17:53:32 -0500

    set up new dashboard css grid for sidebar nav

--- frontend/src/components/views/Dashboard/index.js
+++ frontend/src/components/views/Dashboard/index.js
@@ -5,8 +5,17 @@ import React from 'react'
 const Dashboard = (props) => {
 
   return(
-    <div>
-      Dashboard
+    <div id="dashboard">
+      <div id="left-space"></div>
+      <div id="dash-main">
+        <div id="dash-sidebar">
+          SIDEBAR
+        </div>
+        <div id="dash-content">
+          CONTENT
+        </div>
+      </div>
+      <div id="right-space"></div>
     </div>
   )
 }
