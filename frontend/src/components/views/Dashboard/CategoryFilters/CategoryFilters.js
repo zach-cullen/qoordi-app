0ebf90dd00701dd20df0d36d322a54abc5f980cf
Zach Cullen <zacullen@gmail.com>
2020-06-22 14:50:39 -0500

    add material icons to category filters and style

--- frontend/src/components/views/Dashboard/CategoryFilters/CategoryFilters.js
+++ frontend/src/components/views/Dashboard/CategoryFilters/CategoryFilters.js
@@ -1,5 +1,6 @@
 import './CategoryFilters.css'
 import React from 'react'
+import MaterialIcon from 'material-icons-react'
 
 const CategoryFilters = () => {
   return(
@@ -9,25 +10,25 @@ const CategoryFilters = () => {
       </div>
 
       <div className="category-filter">
-        <span className="category-filter-toggle"></span>
+        <span className="category-filter-icon" style={{color: "#E774BF"}}><MaterialIcon icon="toggle_on" size={20} color="inherit" /></span>
         <span className="category-filter-text">Wedding</span>
         <span className="category-filter-options"></span>
       </div>
 
       <div className="category-filter">
-        <span className="category-filter-toggle"></span>
+        <span className="category-filter-icon" style={{color: "#FA945C"}}><MaterialIcon icon="toggle_on" size={20} color="inherit" /></span>
         <span className="category-filter-text">Corporate</span>
         <span className="category-filter-options"></span>
       </div>
 
       <div className="category-filter">
-        <span className="category-filter-toggle"></span>
+        <span className="category-filter-icon" style={{color: "gray"}}><MaterialIcon icon="toggle_off" size={20} color="inherit"/></span>
         <span className="category-filter-text">Music Video</span>
         <span className="category-filter-options"></span>
       </div>
 
       <div className="category-filter">
-        <span className="category-filter-toggle"></span>
+        <span className="category-filter-icon" style={{color: "gray"}}><MaterialIcon icon="add" size={20} color="inherit"/></span>
         <span className="category-filter-text">New Category</span>
         <span className="category-filter-options"></span>
       </div>
