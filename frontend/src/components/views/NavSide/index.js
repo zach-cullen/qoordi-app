b81b455e42c503f3da73329ac72e8ce0d7562fbb
Zach Cullen <zacullen@gmail.com>
2020-06-21 19:47:05 -0500

    add MaterialIcon components and styles to NavLinks

--- frontend/src/components/views/NavSide/index.js
+++ frontend/src/components/views/NavSide/index.js
@@ -2,6 +2,7 @@ import './NavSide.css'
 import { ReactComponent as Logo } from '../../logo/qoordi-logo.svg'
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import MaterialIcon from 'material-icons-react'
 
 const NavSide = () => {
 
@@ -9,12 +10,21 @@ const NavSide = () => {
     <nav id="nav-side">
       <NavLink className="nav-side-home" exact to="/"><Logo /></NavLink>
       <div className="nav-side-links">
+
         <NavLink className="nav-side-link" exact to="/app">
-          Dashboard
+          <span class="nav-side-link-icon">
+            <MaterialIcon icon="calendar_today" size={20} color="inherit"/>
+          </span>
+          <span class="nav-side-link-text">Dashboard</span>
         </NavLink>
+
         <NavLink className="nav-side-link" exact to="/logout">
-          Log out
+          <span class="nav-side-link-icon">
+            <MaterialIcon icon="exit_to_app" size={20} color="inherit"/>
+          </span>
+          <span class="nav-side-link-text">Log out</span>
         </NavLink>
+        
       </div>
     </nav>
   )
