a977ea357508593a447d185dbeede85fb19b033e
Zach Cullen <zacullen@gmail.com>
2020-06-22 16:09:30 -0500

    add projectsReducer to entitiesReducer

--- frontend/src/reducers/entitiesReducer.js
+++ frontend/src/reducers/entitiesReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import usersReducer from './usersReducer'
+import projectsReducer from './projectsReducer'
 
 const entitiesReducer = combineReducers({
-  users: usersReducer
+  users: usersReducer,
+  projects: projectsReducer,
 })
 
 export default entitiesReducer
