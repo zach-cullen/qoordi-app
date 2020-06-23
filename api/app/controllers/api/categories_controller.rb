753849965fb8262f0690db2e2e2b990b2b54ab7e
Zach Cullen <zacullen@gmail.com>
2020-06-23 16:45:49 -0500

    create strong_params for categories

--- api/app/controllers/api/categories_controller.rb
+++ api/app/controllers/api/categories_controller.rb
@@ -1,7 +1,14 @@
 class Api::CategoriesController < Api::ApiController
 
   def create
-    byebug
+    # @category = @current_user.categories.build(category_params)
+    # byebug
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:title, :color)
   end
 
 end
