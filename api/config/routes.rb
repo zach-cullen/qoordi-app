9a0f1fedb025dd3e45e81f1af3b26885a6b598e7
Zach Cullen <zacullen@gmail.com>
2020-06-23 16:45:20 -0500

    create categories#create route

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :users, only: [:show, :create, :update, :delete]
+    resources :categories, only: [:create]
     post "/login", to: "sessions#create"
     post "/logout", to: "sessions#destroy"
   end
