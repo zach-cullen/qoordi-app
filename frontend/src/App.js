25c56c268087e16aebe76bc713ab1bfea692b4a3
Zach Cullen <zacullen@gmail.com>
2020-06-23 08:47:57 -0500

    load option-color classes and use for categoryFilter styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './styles/containers.css'
 import './styles/forms.css'
 import './styles/buttons.css'
 import './styles/spacers.css'
+import './styles/option-colors.css'
 
 import React from 'react';
 import Routes from './routes'
--- frontend/src/components/views/Dashboard/CategoryFilters/CategoryFilter.js
+++ frontend/src/components/views/Dashboard/CategoryFilters/CategoryFilter.js
@@ -4,7 +4,9 @@ import MaterialIcon from 'material-icons-react'
 const CategoryFilter = ({ category }) => {
   return(
     <div className="category-filter">
-      <span className="category-filter-icon" style={{color: "#E774BF"}}><MaterialIcon icon="toggle_on" size={20} color="inherit" /></span>
+      <span className={`category-filter-icon option-font-${category.color}`}>
+        <MaterialIcon icon="toggle_on" size={20} color="inherit" />
+      </span>
       <span className="category-filter-text">{category.title}</span>
       <span className="category-filter-options"></span>
     </div>
