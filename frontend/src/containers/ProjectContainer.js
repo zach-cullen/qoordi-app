24e30adf36ad2597c194ce931bcb4f0cc5ec2abc
Zach Cullen <zacullen@gmail.com>
2020-06-24 14:10:44 -0500

    create basic component ProjectContainer

---
+++ frontend/src/containers/ProjectContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class ProjectContainer extends Component {
+
+  render() {
+    return(
+      <div className="main">
+        ProjectContainer
+      </div>
+    )
+  }
+
+}
+
+export default ProjectContainer
