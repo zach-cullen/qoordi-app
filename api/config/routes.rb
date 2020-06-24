d38bd2689d2800c3ec96a8e939b3308840842e0e
Zach Cullen <zacullen@gmail.com>
2020-06-24 16:13:14 -0500

    create projects#show route

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :users, only: [:show, :create, :update, :delete]
     resources :categories, only: [:create]
-    resources :projects, only: [:create]
+    resources :projects, only: [:show, :create]
     post "/login", to: "sessions#create"
     post "/logout", to: "sessions#destroy"
   end
