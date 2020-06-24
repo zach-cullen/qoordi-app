ac6002dab42f815e44049ece411f1ef264b35547
Zach Cullen <zacullen@gmail.com>
2020-06-24 13:47:41 -0500

    create project for current user and render json response

--- api/app/controllers/api/projects_controller.rb
+++ api/app/controllers/api/projects_controller.rb
@@ -1,13 +1,40 @@
 class Api::ProjectsController < Api::ApiController
 
   def create
-    byebug
+    @project = @current_user.projects.build(
+      title: project_params[:title], 
+      date: parse_date_from_project_params
+    )
+    @project.category = valid_category
+    
+    if @current_user.save && @current_user.projects.find_by(id: @project.id)
+      render json: {
+        project_created: true,
+        project: @project.as_json(except: [:created_at, :updated_at])
+      }
+    else
+      render json: {
+        project_created: false,
+        errors: "Project creation was unsuccessful."
+      }
+    end
   end
 
   private
 
-  # def project_params
+  def parse_date_from_project_params
+    require 'time'
+    Date.parse(project_params[:date])
+  end
 
-  # end
+  # returns either a valid category that belongs to current user or the user's first category
+  def valid_category
+    category = @current_user.categories.find_by(id: project_params[:category_id])
+    category ? category : @current_user.categories.first
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :date, :category_id)
+  end
 
 end
