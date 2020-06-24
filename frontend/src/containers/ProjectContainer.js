8ece263e7ee75b7445b019599f2f855b7fb37007
Zach Cullen <zacullen@gmail.com>
2020-06-24 16:17:25 -0500

    fetchProject from api after component mounts

--- frontend/src/containers/ProjectContainer.js
+++ frontend/src/containers/ProjectContainer.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchProject } from '../actions/projectsActions'
 
 class ProjectContainer extends Component {
+
+  componentDidMount() {
+    const projectId = this.props.match.params.id
+    fetchProject({ id: projectId })
+    // set state of this component to loading: true
+    // display loading screen until redux store contains project with id that matches
+    // then => dispatch action getProject that fetches project data from api and adds to redux store
+    // 
+  }
 
   render() {
     return(
@@ -9,7 +20,12 @@ class ProjectContainer extends Component {
       </div>
     )
   }
-
 }
 
-export default ProjectContainer
+const mapStateToProps = (state) => {
+  return {
+    projects: state.entities.projects
+  }
+}
+
+export default connect (mapStateToProps)(ProjectContainer)
