c3929af143f8f64650311cd72e40d0b2a2a31b86
Zach Cullen <zacullen@gmail.com>
2020-06-24 08:23:29 -0500

    save category from category_params and render json response

--- api/app/controllers/api/categories_controller.rb
+++ api/app/controllers/api/categories_controller.rb
@@ -1,8 +1,20 @@
 class Api::CategoriesController < Api::ApiController
 
   def create
-    # @category = @current_user.categories.build(category_params)
-    # byebug
+    @category = @current_user.categories.build(category_params)
+    
+    if @category.save
+      render json: {
+        category_created: true,
+        category: @category.as_json(except: [:created_at, :updated_at])
+      }
+    else
+      render json: {
+        category_created: false,
+        errors: "Category creation was unsuccessful."
+      }
+    end
+
   end
 
   private
