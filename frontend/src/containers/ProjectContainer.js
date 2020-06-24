6eb8b96cd1cd2e4d19a49a071f942cb2afde44d2
Zach Cullen <zacullen@gmail.com>
2020-06-24 16:26:45 -0500

    map dispatch to fetchProject

--- frontend/src/containers/ProjectContainer.js
+++ frontend/src/containers/ProjectContainer.js
@@ -6,7 +6,7 @@ class ProjectContainer extends Component {
 
   componentDidMount() {
     const projectId = this.props.match.params.id
-    fetchProject({ id: projectId })
+    this.props.fetchProject({ id: projectId })
     // set state of this component to loading: true
     // display loading screen until redux store contains project with id that matches
     // then => dispatch action getProject that fetches project data from api and adds to redux store
@@ -28,4 +28,12 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect (mapStateToProps)(ProjectContainer)
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchProject: (project) => {
+      dispatch(fetchProject(project))
+    }
+  }
+} 
+
+export default connect (mapStateToProps, mapDispatchToProps)(ProjectContainer)
