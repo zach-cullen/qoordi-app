5a5fa08330cd694164a86fb0eaefb4bd98cfa842
Zach Cullen <zacullen@gmail.com>
2020-06-25 08:58:46 -0500

    render home logo link in project sidebar

--- frontend/src/components/views/ProjectView/ProjectSideBar/ProjectSideBar.js
+++ frontend/src/components/views/ProjectView/ProjectSideBar/ProjectSideBar.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { ReactComponent as Logo } from '../../../logo/qoordi-logo.svg'
 
 const ProjectSideBar = () => {
 
   return (
     <div id="project-view-sidebar">
       <div id="project-view-sidebar-container">
+        <NavLink className="nav-side-home" exact to="/"><Logo /></NavLink>
       </div>
     </div>
   )
-  
+
 }
 
 export default ProjectSideBar
