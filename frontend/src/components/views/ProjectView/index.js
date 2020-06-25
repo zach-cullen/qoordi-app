c1e87a3546e5c64a8a2c4cbec6044563dbcd80fd
Zach Cullen <zacullen@gmail.com>
2020-06-25 08:01:28 -0500

    create component ProjectView

---
+++ frontend/src/components/views/ProjectView/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProjectView = (props) => {
+
+  return(
+    <div id="project-view">
+      <h1>Project View</h1>
+    </div>
+  )
+}
+
+export default ProjectView
