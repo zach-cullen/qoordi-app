947b66568b4d48d2b582fcda52ddeabfbb122eef
Zach Cullen <zacullen@gmail.com>
2020-06-26 11:20:31 -0500

    convert Timeline to class component

--- frontend/src/components/views/ProjectView/Timelines/Timeline.js
+++ frontend/src/components/views/ProjectView/Timelines/Timeline.js
@@ -1,51 +1,54 @@
-import React from 'react'
+import React, { Component } from 'react'
 import TimeBlock from './TimeBlock'
 
-const Timeline = (props) => {
+class Timeline extends Component {
 
-  const timeBlocks = {
-    byId: {
-      1: {
-        id: 1,
-        start: 950,
-        end: 1050,
-        color: "pink"
+  timeBlocks = () => {
+    return {
+      byId: {
+        1: {
+          id: 1,
+          start: 950,
+          end: 1050,
+          color: "pink"
+        },
+        2: {
+          id: 2,
+          start: 1100,
+          end: 1150,
+          color: "blue",
+        },
+        3: {
+          id: 3,
+          start: 1300,
+          end: 1450,
+          color: "orange",
+        }
       },
-      2: {
-        id: 2,
-        start: 1100,
-        end: 1150,
-        color: "blue",
-      },
-      3: {
-        id: 3,
-        start: 1300,
-        end: 1450,
-        color: "orange",
-      }
-    },
-    allIds: [1, 2, 3]
+      allIds: [1, 2, 3]
+    }
   }
 
-  const mapTimeBlocks = (timeBlocks) => {
+  mapTimeBlocks = (timeBlocks) => {
     return timeBlocks.allIds.map((i) => {
       return(
         <TimeBlock key={i} 
-          projectStart={props.startTime}
-          projectEnd={props.endTime}
-          timeBlock={timeBlocks.byId[i]} 
+          projectStart={this.props.startTime}
+          projectEnd={this.props.endTime}
+          timeBlock={this.timeBlocks().byId[i]} 
         />
       )
     })
   }
 
 
-
-  return(
-    <div className="timeline">
-      { mapTimeBlocks(timeBlocks) }
-    </div>
-  )
+  render() {
+    return(
+      <div className="timeline">
+        { this.mapTimeBlocks(this.timeBlocks()) }
+      </div>
+    )
+  }
 }
 
 export default Timeline
