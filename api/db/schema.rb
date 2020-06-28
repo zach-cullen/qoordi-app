34306836754b555ac2855334f339c3d22d819245
Zach Cullen <zacullen@gmail.com>
2020-06-28 18:13:32 -0500

    migrate create timeblocks table

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_28_210855) do
+ActiveRecord::Schema.define(version: 2020_06_28_231017) do
 
   create_table "categories", force: :cascade do |t|
     t.integer "user_id"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 2020_06_28_210855) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id"
+  end
+
+  create_table "timeblocks", force: :cascade do |t|
+    t.integer "timeline_id"
+    t.string "title"
+    t.text "description"
+    t.string "start_time"
+    t.string "end_time"
+    t.string "color"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "timelines", force: :cascade do |t|
