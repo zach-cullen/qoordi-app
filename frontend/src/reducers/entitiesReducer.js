1eef7d026b914b6bea29a0cb719a42c8b48d5c8a
Zach Cullen <zacullen@gmail.com>
2020-06-28 16:49:06 -0500

    add timelines to state by combining timelinesReducer in entitiesReducer

--- frontend/src/reducers/entitiesReducer.js
+++ frontend/src/reducers/entitiesReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import usersReducer from './usersReducer'
 import projectsReducer from './projectsReducer'
 import categoriesReducer from './categoriesReducer'
+import timelinesReducer from './timelinesReducer'
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   projects: projectsReducer,
   categories: categoriesReducer,
+  timelines: timelinesReducer,
 })
 
 export default entitiesReducer
