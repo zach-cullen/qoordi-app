d6a90eb5551dd8613dfd431a41fd9c314c0fa516
Zach Cullen <zacullen@gmail.com>
2020-06-29 15:09:02 -0500

    create NewTimeBlockForm class component rendering simple div

---
+++ frontend/src/components/views/ProjectView/ProjectSideBar/NewTimeBlockForm.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class NewTimeBlockForm extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <div className="sidebar-form" id="new-time-block-form">
+        FORM
+      </div>
+    )
+  }
+
+}
+
+export default NewTimeBlockForm
