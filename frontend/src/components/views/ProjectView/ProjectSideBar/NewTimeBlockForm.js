9f4c79b70cc48ee67bdaac864be6364fef2c3f34
Zach Cullen <zacullen@gmail.com>
2020-06-29 16:40:27 -0500

    render timeblock form with custom css classes

--- frontend/src/components/views/ProjectView/ProjectSideBar/NewTimeBlockForm.js
+++ frontend/src/components/views/ProjectView/ProjectSideBar/NewTimeBlockForm.js
@@ -5,14 +5,36 @@ class NewTimeBlockForm extends Component {
   constructor() {
     super()
     this.state = {
-
+      title: "",
+      description: "",
     }
   }
 
   render() {
     return(
       <div className="sidebar-form" id="new-time-block-form">
-        FORM
+        <form className="sidebar-form-container">
+          <label>
+            NEW EVENT
+            <input 
+              type="text" name="title"
+              onChange={this.handleChange} 
+              value={this.state.title} 
+              placeholder="Add Title"
+              autofocus="true"
+            />
+          </label>
+          <br />
+          <label>
+            DESCRIPTION
+            <textarea 
+              type="text-area" name="description"
+              onChange={this.handleChange} 
+              value={this.state.description} 
+              placeholder="Add Description"
+            />
+          </label>
+        </form>
       </div>
     )
   }
