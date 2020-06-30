f1cb7af1654fd5ca0f74d192a9570e0ca3cb8f9a
Zach Cullen <zacullen@gmail.com>
2020-06-30 11:16:58 -0500

    add timeblocksReducer to entities

--- frontend/src/reducers/entitiesReducer.js
+++ frontend/src/reducers/entitiesReducer.js
@@ -3,12 +3,14 @@ import usersReducer from './usersReducer'
 import projectsReducer from './projectsReducer'
 import categoriesReducer from './categoriesReducer'
 import timelinesReducer from './timelinesReducer'
+import timeblocksReducer from './timeblocksReducer'
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   projects: projectsReducer,
   categories: categoriesReducer,
   timelines: timelinesReducer,
+  timeblocks: timeblocksReducer,
 })
 
 export default entitiesReducer
