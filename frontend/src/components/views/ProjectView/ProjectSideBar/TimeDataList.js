ffec49287ecd347653d06267b90fd40426674304
Zach Cullen <zacullen@gmail.com>
2020-06-30 09:11:19 -0500

    map fake start and end time props to datalist options

--- frontend/src/components/views/ProjectView/ProjectSideBar/TimeDataList.js
+++ frontend/src/components/views/ProjectView/ProjectSideBar/TimeDataList.js
@@ -1,17 +1,63 @@
 import React from 'react'
+import HourLabels from '../HourLabels/HourLabels'
 
 const TimeDataList = (props) => {
 
+  // fake props from project
+  const startTime = "09:00"
+  const endTime = "23:00"
+
+  const renderOptions = () => {
+
+    // parse start and end hr integers from strings
+    const startHr = parseInt(startTime.slice(0, 2))
+    const endHr = parseInt(endTime.slice(0, 2))
+
+    // create array of integers representing each hour from start to end (inclusive)
+    const hrsArray = Array.from(Array(endHr - startHr + 1), (x, i) => i + startHr)
+
+    // create array of time strings from start time to end time
+    const hrsToTimes = () => {
+      return hrsArray.map((hr, i) => {
+        // convert hr (24 hr scale) to 12hr time scale and make string
+        const hrTo12 = hr <= 12 ? hr.toString() : (hr - 12).toString()
+
+        // returns string representing hour in am / pm 12 hr scale
+        const amOrPm = () => {
+          if (hr < 12) {
+            return "AM"
+          }
+          if (hr === 24) {
+            return "AM"
+          }
+          return "PM"
+        }
+
+        // return only hour if last hr
+        if (i === hrsArray.length - 1) {
+          return [`${hrTo12}:00 ${amOrPm()}`]
+        }
+
+        // return array of times on 15 min increments (flattened later)
+        return [
+          `${hrTo12}:00 ${amOrPm()}`, 
+          `${hrTo12}:15 ${amOrPm()}`, 
+          `${hrTo12}:30 ${amOrPm()}`, 
+          `${hrTo12}:45 ${amOrPm()}`
+        ]
+      }).flat()
+    }
+
+    return hrsToTimes().map((time, i) => {
+      return(
+        <option key={i} value={time} />
+      )
+    })
+  }
+
   return(
     <datalist id="times">
-      <option value="1:00am" />
-      <option value="1:15am" />
-      <option value="1:30am" />
-      <option value="1:45am" />
-      <option value="2:00am" />
-      <option value="3:00am" />
-      <option value="4:00am" />
-      <option value="5:00am" />
+      { renderOptions() }
     </datalist>
   )
 }
