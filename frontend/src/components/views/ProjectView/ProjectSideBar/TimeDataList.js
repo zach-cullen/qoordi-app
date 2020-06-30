3503579cd9849e092bbe13f79ba57951a808307e
Zach Cullen <zacullen@gmail.com>
2020-06-30 07:50:55 -0500

    separate datalist into its own component

---
+++ frontend/src/components/views/ProjectView/ProjectSideBar/TimeDataList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const TimeDataList = (props) => {
+
+  return(
+    <datalist id="times">
+      <option value="1:00am" />
+      <option value="1:15am" />
+      <option value="1:30am" />
+      <option value="1:45am" />
+      <option value="2:00am" />
+      <option value="3:00am" />
+      <option value="4:00am" />
+      <option value="5:00am" />
+    </datalist>
+  )
+}
+
+export default TimeDataList
