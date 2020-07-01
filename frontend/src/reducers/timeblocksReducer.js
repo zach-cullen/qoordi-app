d772fa990917d577095e6925ace57b756d2794ca
Zach Cullen <zacullen@gmail.com>
2020-06-30 21:35:25 -0500

    add case update new timeblock times that changes the start and end time of timeBlock by id

--- frontend/src/reducers/timeblocksReducer.js
+++ frontend/src/reducers/timeblocksReducer.js
@@ -16,6 +16,21 @@ const timeblocksReducer = (state = {
         allIds: state.allIds.includes(timeblockData.id) ? state.allIds : state.allIds.concat(timeblockData.id)
       }
 
+    case "UPDATE_NEW_TIMEBLOCK_TIMES":
+      const block = action.payload.timeblock
+      return {
+        ...state,
+        byId: {
+          ...state.byId,
+          [block.id]: {
+            ...state.byId[block.id],
+            start_time: block.start_time,
+            end_time: block.end_time,
+          }
+        }
+      }
+
+
     default:
       return state
   }
