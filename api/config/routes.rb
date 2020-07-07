03e1fac0859190a0dc15dd3e3ea340d43ec8ab48
Zach Cullen <zacullen@gmail.com>
2020-07-07 09:24:28 -0500

    create update resource route for timeblocks

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :users, only: [:show, :create, :update, :delete]
     resources :categories, only: [:create]
     resources :projects, only: [:show, :create]
-    resources :timeblocks, only: [:create]
+    resources :timeblocks, only: [:create, :update]
     post "/login", to: "sessions#create"
     post "/logout", to: "sessions#destroy"
   end
