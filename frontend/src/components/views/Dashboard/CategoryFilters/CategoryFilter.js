1f97bebec532a17583ae8ad392ef76a5bf74c605
Zach Cullen <zacullen@gmail.com>
2020-07-07 14:37:31 -0500

    replace MaterialIcon component with ToggleOnIcon

--- frontend/src/components/views/Dashboard/CategoryFilters/CategoryFilter.js
+++ frontend/src/components/views/Dashboard/CategoryFilters/CategoryFilter.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import MaterialIcon from 'material-icons-react'
+import ToggleOnIcon from '@material-ui/icons/ToggleOn'
 
 const CategoryFilter = ({ category }) => {
   return(
     <div className="category-filter">
       <span className={`category-filter-icon option-font-${category.color}`}>
-        <MaterialIcon icon="toggle_on" size={20} color="inherit" />
+        <ToggleOnIcon fontSize="inherit" color="inherit" />
       </span>
       <span className="category-filter-text">{category.title}</span>
       <span className="category-filter-options"></span>
