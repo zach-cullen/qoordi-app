f807f8c6f55fc64c862183d374c434d0d38dede5
Zach Cullen <zacullen@gmail.com>
2020-07-07 14:38:15 -0500

    replace MaterialIcon component with CalendarToday and ExitToApp icons

--- frontend/src/components/views/NavSide/NavSideLinks.js
+++ frontend/src/components/views/NavSide/NavSideLinks.js
@@ -1,21 +1,22 @@
 import './NavSide.css'
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import MaterialIcon from 'material-icons-react'
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 
 const NavSideLinks = () => {
   return(
     <div className="nav-side-links">
       <NavLink className="nav-side-link" exact to="/app">
         <span className="nav-side-link-icon">
-          <MaterialIcon icon="calendar_today" size={20} color="inherit"/>
+          <CalendarTodayIcon fontSize="inherit" color="inherit" />
         </span>
         <span className="nav-side-link-text">Dashboard</span>
       </NavLink>
 
       <NavLink className="nav-side-link" exact to="/logout">
         <span className="nav-side-link-icon">
-          <MaterialIcon icon="exit_to_app" size={20} color="inherit"/>
+          <ExitToAppIcon fontSize="inherit" color="inherit" />
         </span>
         <span className="nav-side-link-text">Log out</span>
       </NavLink>
