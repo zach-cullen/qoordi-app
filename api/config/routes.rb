dfa73f2a50125360f2516d22bf0e1d992eaee54b
Zach Cullen <zacullen@gmail.com>
2020-07-08 10:52:35 -0500

    remove unused user update and destroy routes, add destroy route to timeblock

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :users, only: [:show, :create, :update, :delete]
+    resources :users, only: [:show, :create]
     resources :categories, only: [:create]
     resources :projects, only: [:show, :create]
-    resources :timeblocks, only: [:create, :update]
+    resources :timeblocks, only: [:create, :update, :destroy]
     post "/login", to: "sessions#create"
     post "/logout", to: "sessions#destroy"
   end
