a053a310868e80782b69b05000a033a7a16d1f20
Zach Cullen <zacullen@gmail.com>
2020-07-08 16:20:21 -0500

    render toggleOn or toggleOff icon and set icon color based on icon hidden prop

--- frontend/src/components/views/Dashboard/CategoryFilters/CategoryFilter.js
+++ frontend/src/components/views/Dashboard/CategoryFilters/CategoryFilter.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ToggleOnIcon from '@material-ui/icons/ToggleOn'
+import ToggleOffIcon from '@material-ui/icons/ToggleOff'
 
 const CategoryFilter = (props) => {
 
@@ -7,13 +8,31 @@ const CategoryFilter = (props) => {
     props.toggleHideCategory(props.category.id)
   }
 
+  const renderToggleIcon = () => {
+    if (!props.categoryHidden) {
+      return(
+        <ToggleOnIcon fontSize="inherit" color="inherit" />
+      )
+    }
+
+    if (props.categoryHidden) {
+      return(
+        <ToggleOffIcon fontSize="inherit" color="inherit" />
+      )
+    }
+  } 
+
+  const setIconColor = () => {
+    return !props.categoryHidden ? props.category.color : "gray"
+  }
+
   return(
     <div className="category-filter">
       <span 
-        className={`category-filter-icon option-font-${props.category.color}`}
+        className={`category-filter-icon option-font-${setIconColor()}`}
         onClick={handleClick}
       >
-        <ToggleOnIcon fontSize="inherit" color="inherit" />
+        {renderToggleIcon()}
       </span>
       <span className="category-filter-text">{props.category.title}</span>
       <span className="category-filter-options"></span>
