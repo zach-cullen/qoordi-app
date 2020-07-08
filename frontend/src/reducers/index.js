d149ccdb27da9878e71aec6fff02358e0a4d3a6e
Zach Cullen <zacullen@gmail.com>
2020-07-08 12:17:54 -0500

    allow rootReducer to receive actions so that it can reset state on logout

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,10 +2,18 @@ import { combineReducers } from 'redux'
 import { sessionReducer } from 'redux-react-session'
 import entitiesReducer from './entitiesReducer'
 
-
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   session: sessionReducer,
   entities: entitiesReducer,
 })
+
+const rootReducer = (state, action) => {
+  if (action.type === "RESET_STORE") {
+    state = {}
+    return appReducer(state, action)
+  }
+
+  return appReducer(state, action)
+}
 
 export default rootReducer
