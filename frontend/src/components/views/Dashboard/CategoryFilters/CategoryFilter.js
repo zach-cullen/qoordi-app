ea5c0d8beba67468b933a43f6e6b5cb79504aa34
Zach Cullen <zacullen@gmail.com>
2020-07-08 16:03:55 -0500

    handle icon click by calling prop function toggleHideCategory

--- frontend/src/components/views/Dashboard/CategoryFilters/CategoryFilter.js
+++ frontend/src/components/views/Dashboard/CategoryFilters/CategoryFilter.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import ToggleOnIcon from '@material-ui/icons/ToggleOn'
 
-const CategoryFilter = ({ category }) => {
+const CategoryFilter = (props) => {
+
+  const handleClick = () => {
+    props.toggleHideCategory(props.category.id)
+  }
+
   return(
     <div className="category-filter">
-      <span className={`category-filter-icon option-font-${category.color}`}>
+      <span 
+        className={`category-filter-icon option-font-${props.category.color}`}
+        onClick={handleClick}
+      >
         <ToggleOnIcon fontSize="inherit" color="inherit" />
       </span>
-      <span className="category-filter-text">{category.title}</span>
+      <span className="category-filter-text">{props.category.title}</span>
       <span className="category-filter-options"></span>
     </div>
   )
