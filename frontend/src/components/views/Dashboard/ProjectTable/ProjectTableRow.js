03ec0334e84beef31ff8b51e07b6df454f1842e2
Zach Cullen <zacullen@gmail.com>
2020-07-20 11:36:53 -0500

    clean up render category function and add comments

--- frontend/src/components/views/Dashboard/ProjectTable/ProjectTableRow.js
+++ frontend/src/components/views/Dashboard/ProjectTable/ProjectTableRow.js
@@ -2,20 +2,24 @@ import React from 'react'
 
 const ProjectTableRow = (props) => {
 
-  const categoryLoaded = !!props.category
-
-  const renderCategory = (category) => {
-    return(
-      <div>
-        <span className={`category-color-spot option-bg-${category.color}`}></span>
-        <span>{category.title}</span>
-      </div>
-    )
+  const renderCategory = () => {
+    // wait for category to load before rendering title and color in case project loads before category in redux state
+    if (!!props.category) {
+      return(
+        <div>
+          <span className={`category-color-spot option-bg-${props.category.color}`}></span>
+          <span>{props.category.title}</span>
+        </div>
+      )
+    } else {
+      return ""
+    }
   }
 
+  // uses inbuilt Date functions that return array indexes to display day of week and month as pretty format in table row
   const dateStringPrettyFormat = (dateString) => {
     const date = new Date(dateString)
-    const days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"]
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     const pretty = `${days[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
     return pretty
@@ -30,7 +34,7 @@ const ProjectTableRow = (props) => {
         { dateStringPrettyFormat(props.project.date) }
       </div>
       <div>
-        { categoryLoaded ? renderCategory(props.category) : ""}
+        { renderCategory() }
       </div>
       <div>
       </div>
